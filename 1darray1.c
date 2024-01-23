f750f308459f97ec5254d72f56abd7c1c938a6d3
Rohit sahu <143478312+Rohit20115@users.noreply.github.com>
2024-01-23 19:59:05 +0530

    Add files via upload

---
+++ 1darray1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+
+        int a[5],i;
+
+        printf("\n enter array element : ");
+        
+        for(i=0;i<5;i++)
+        {
+
+                scanf("%d",&a[i]);
+
+        }
+
+        for(i=0;i<5;i++)
+
+        {
+
+            printf("%d\t",a[i]);
+
+        }
+}
